c7a56e096be52f76f709b95671f7412c9076f382
Marc Kohaupt <debuglevel@gmail.com>
2019-09-06 18:50:18 +0200

    Log authentication attempts

--- src/main/kotlin/de/debuglevel/activedirectory/UserPasswordAuthenticationProvider.kt
+++ src/main/kotlin/de/debuglevel/activedirectory/UserPasswordAuthenticationProvider.kt
@@ -3,6 +3,7 @@ package de.debuglevel.activedirectory
 import io.micronaut.context.annotation.Property
 import io.micronaut.security.authentication.*
 import io.reactivex.Flowable
+import mu.KotlinLogging
 import org.reactivestreams.Publisher
 import java.util.*
 import javax.inject.Singleton
@@ -15,12 +16,18 @@ class UserPasswordAuthenticationProvider(
     @Property(name = "app.security.username") private val username: String,
     @Property(name = "app.security.password") private val password: String
     ) : AuthenticationProvider {
+    private val logger = KotlinLogging.logger {}
+
     override fun authenticate(authenticationRequest: AuthenticationRequest<*, *>): Publisher<AuthenticationResponse> {
+//        logger.debug { "Got request with username '${authenticationRequest.identity}' and password '${authenticationRequest.secret}'" }
         if (authenticationRequest.identity == username &&
             authenticationRequest.secret == password
         ) {
+            logger.debug { "Credentials are valid" }
             return Flowable.just(UserDetails(username, ArrayList()))
         } else {
+            logger.debug { "Credentials are invalid" }
+//            logger.debug { "Credentials are invalid: '${authenticationRequest.identity}' != '$username', '${authenticationRequest.secret}' != '$password'" }
             return Flowable.just(AuthenticationFailed())
         }
     }
